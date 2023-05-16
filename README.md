501cf75a06037ba715212616a78b2aac8ce90868
H0riaa <noua.aprilie@yahoo.com>
2023-05-17 02:57:36 +0300

    Changed to english & Updated README

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-"# Tic-Tac-Toe" 
-"# Tic-Tac-Toe" 
-"# Tic-Tac-Toe" 
+## About   
+This is my first project in C++ implementing a simple Tic Tac Toe, against the computer.   
+The seed is set to random when the application is opened.   
+
--- main.cpp
+++ main.cpp
@@ -2,68 +2,73 @@
 #include <time.h>
 using namespace std;
 
+#define ROCK 1
+#define PAPER 2
+#define SCISSORS 3
 int main()
 {
     int user;
     int cpu;
     srand(time(NULL));
 
-    cout << "Piatra = 1\n";
-    cout << "Hartie = 2\n";
-    cout << "Foarfeca = 3\n";
+    cout << "Rock = 1\n";
+    cout << "Paper = 2\n";
+    cout << "Scissors = 3\n";
     cin >> user;
     cpu = (rand() % 3 + 1);
 
-    if(user == 1 && cpu == 1)
+    if(user == cpu)
     {
-        cout << "Amandoi ati ales piatra.\n";
-        cout << "Este egalitate!!\n";
-    }
-    else if(user == 2 && cpu == 2)
-    {
-        cout << "Amandoi ati ales hartie.\n";
-        cout << "Este egalitate!!\n";
-    }
-    else if(user == 3 && cpu == 3)
-    {
-        cout << "Amandoi ati ales foarfeca.\n";
-        cout << "Este Egalitate";
+        if(user == ROCK)
+        {
+            cout << "You both chose rock.\n";
+        }
+        else if(user == PAPER)
+        {
+            cout << "You both chose paper.\n";
+        }
+        else
+        {
+            cout << "You both chose scissors.\n";
+        }
+        
+        cout << "It's a draw!!\n";
     }
     else if(user == 1 && cpu == 3)
     {
-        cout << "Ai ales piatra,\n";
-        cout << "CPU a ales foarfeca\n";
-        cout << "Ai castigat!!!\n";
+        cout << "You chose rock,\n";
+        cout << "CPU chose scissors\n";
+        cout << "You won!!!\n";
     }
     else if(user == 2 && cpu == 1)
     {
-        cout << "Ai ales hartie,\n";
-        cout << "CPU a ales foarfeca\n";
-        cout << "Ai castigat!!!\n";
+        cout << "You chose paper,\n";
+        cout << "CPU chose rock\n";
+        cout << "You won!!!\n";
     }
     else if(user == 3 && cpu == 2)
     {
-        cout << "Ai ales foarfeca,\n";
-        cout << "CPU a ales hartie\n";
-        cout << "Ai castigat!!!\n";
+        cout << "You chose scissors,\n";
+        cout << "CPU chose paper\n";
+        cout << "You won!!!\n";
     }
     else if(user == 1 && cpu == 2)
     {
-        cout << "Ai ales piatra,\n";
-        cout << "CPU a ales hartie\n";
-        cout << "CPU a castigat!!!\n";
+        cout << "You chose rock,\n";
+        cout << "CPU chose paper\n";
+        cout << "CPU won!!!\n";
     }
     else if(user == 2 && cpu == 3)
     {
-        cout << "Ai ales hartie,\n";
-        cout << "CPU a ales foarfeca\n";
-        cout << "CPU a castigat!!!\n";
+        cout << "You chose paper,\n";
+        cout << "CPU chose scissors\n";
+        cout << "CPU won!!!\n";
     }
     else if(user == 3 && cpu == 1)
     {
-        cout << "Ai ales foarfeca,\n";
-        cout << "CPU a ales piatra\n";
-        cout << "CPU a castigat!!!\n";  
+        cout << "You chose scissors,\n";
+        cout << "CPU chose rock\n";
+        cout << "CPU won!!!\n";  
     }
 
 }
